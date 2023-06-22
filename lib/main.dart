55921f6e19754205cb589640c69c78673c126fa2
Niraj Shrestha <niraj.shrestha@wesionary.team>
2023-06-22 17:04:46 +0545

    chore: error

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      \uawdiu
       title: 'Custom Animations app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
