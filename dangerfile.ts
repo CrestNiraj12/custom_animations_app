beac293aa6f24b079451555bb2c3db58d36521e8
Niraj Shrestha <crestniraj@gmail.com>
2024-06-19 15:02:06 +0545

    alksjndlnkjsad

--- dangerfile.ts
+++ dangerfile.ts
@@ -16,31 +16,8 @@ function checkPRSize() {
 }
 
 function runFlutterAnalyzer() {
-  const analyzeOutput = child_process.execSync('flutter analyze', { encoding: 'utf-8' });
-  warn(analyzeOutput);
-
   try {
-    const lines = analyzeOutput.split('\n');
-    // const issueRegex = /^(info|warning|error) • (.+) • (.+) • (.+)$/;
-
-    lines.forEach((line) => {
-      warn(line);
-      // const match = line.match(issueRegex);
-      // if (match) {
-      //   const [_, level, msg, file, location] = match;
-      //   switch (level) {
-      //     case 'error':
-      //       fail(`${msg} (${file}:${location})`);
-      //       break;
-      //     case 'warning':
-      //       warn(`${msg} (${file}:${location})`);
-      //       break;
-      //     case 'info':
-      //       message(`${msg} (${file}:${location})`);
-      //       break;
-      //   }
-      // }
-    });
+    child_process.execSync('flutter analyze', { encoding: 'utf-8' });
   } catch (error) {
     fail(`Flutter analyzer failed. Please fix the issues reported by the analyzer. ${error}`)
   }
